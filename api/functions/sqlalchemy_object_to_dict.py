a172916f95fffd66b2cf533a7ccba040a9b55f7f
EmperorOfCoding <victorameno@hotmail.com>
2024-09-24 08:30:00 -0300

    moved

--- api/functions/sqlalchemy_object_to_dict.py
+++
@@ -1,8 +0,0 @@
-from contas_a_pagar_e_receber.models import conta_a_pagar_receber_model
-from sqlalchemy import inspect
-
-
-def sqlalchemy_obj_to_dict(obj):
-
-    return {column.key: getattr(obj, column.key) for column in inspect(obj).mapper.column_attrs}
-
